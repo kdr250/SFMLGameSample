40a7bd031ea0dd0db3ce3c3cb0b5b01a369aa96b
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-04-12 23:22:40 +0900

    windowを作成

--- game/src/Main.cpp
+++ game/src/Main.cpp
@@ -1,8 +1,40 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
+    // Windows
+    sf::RenderWindow window(sf::VideoMode(640, 480),
+                            "SFML Game Sample",
+                            sf::Style::Titlebar | sf::Style::Close);
+    sf::Event event;
+
+    // Game loop
+    while (window.isOpen())
+    {
+        // Event polling
+        while (window.pollEvent(event))
+        {
+            switch (event.type)
+            {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                case sf::Event::KeyPressed:
+                    if (event.key.code == sf::Keyboard::Escape)
+                    {
+                        window.close();
+                    }
+                    break;
+            }
+        }
+
+        // Update
+        window.clear(sf::Color::Blue);  // clear old frame
+
+        // Render
+        window.display();  // tell app that window is done drawing
+    }
 
     return 0;
 }
